8c8e2dfefb18aa4aca2ff1cb91ccd0b65eb402a2
peta999 <checker996@gmx.de>
2025-11-01 22:50:13 +0100

    Remove unused dependencies

--- main/main.cpp
+++ main/main.cpp
@@ -20,8 +20,6 @@ extern "C" {
     #include "freertos/FreeRTOS.h"
     #include "freertos/task.h"
     #include "esp_system.h"
-    #include "driver/i2c.h"
-    #include "../components/shtc1/sensirion_i2c.h"
 }
 
 /**
